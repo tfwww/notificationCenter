93b5f4a72d8f55aeceea5ed8afbeb42543e8479a
Winter Zhong <wmzhong@users.noreply.github.com>
2018-01-19 17:14:49 +0800

    初始功能

---
+++ notificationCenter.js
@@ -0,0 +1,42 @@
+var debug = true        
+var log = debug ? console.log.bind(console) : function() {}
+
+// class
+function NotificationCenter() {
+    // event list
+    this.events = []
+    // event, target and handler pairs
+    this.center = []
+}
+
+NotificationCenter.prototype = {
+    // add event listener
+    addBroadcast: function(eventName, target, handler) {
+        var events = this.events
+        events.push(eventName)
+        
+        var center = this.center
+        var obj = {
+            name: eventName,
+            target: target,
+            handler: handler
+        }
+        center.push(obj)
+
+    },
+
+    // fire event listener
+    fireBrodcast: function(eventName, target) {
+        var mapper = this.center
+        handleWithEvent(mapper, eventName, target)
+    }
+}
+
+function handleWithEvent(array, event, target) {    
+    for (var i = 0; i < array.length; i++) {
+        var item = array[i];
+        if (item['name'] === event && item['target'] === target) {
+            item['handler']()    
+        }
+    }
+}
